98d7afc41b81c622e4ea43cbad0a6688a1457976
“joanomana” <joansebastianomasu@gmail.com>
2024-11-07 17:47:27 -0500

    Exercise 22

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
-#Ejercicio 21: Sistema de estacionamiento con tarifas progresivas
+#Ejercicio 22: Clasificacion de triangulos por sus angulos
 
-print ("Exercise 21")
+print ("Exercise 22")
 print("")
-
-def parking(hours):
-    match hours:
-        case hours if hours >=0 and hours <=1:
-            print("The cost of parking is $5")
-        case hours if hours >=2 and hours <=4:
-            cost = 4*hours
-            print(f"The cost of parking is ${cost}")
-        case hours if hours >4:
-            total_extra = hours - 4
-            cost = 3*total_extra + 16
-            print(f"The cost of parking is ${cost}")
-        case _:
-            print("Invalid data")
-
-
-hours = int(input("Enter the number of hours: "))
-parking(hours)
+angulos = []
+for x in range(3):
+    angulo = int(input("Ingrese el angulo: "))
+    angulos.append(angulo)
+    
+if sum(angulos) != 180:
+        print("Los ángulos no forman un triángulo válido")
+elif all(angulo < 90 for angulo in angulos):
+        print("El triángulo es agudo")
+elif any(angulo == 90 for angulo in angulos):
+        print("El triángulo es rectángulo")
+elif any(angulo > 90 for angulo in angulos):
+        print("El triángulo es obtuso")
