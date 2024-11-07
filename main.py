28119e9b02155c83f4b37bbcfd5b151f1af439f9
“joanomana” <joansebastianomasu@gmail.com>
2024-11-07 11:25:38 -0500

    Exercise 4

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
-#Ejercicio 3: Calculadora Basica
-def calculator (a,b,x):
-    match x:
-        case "+":
-            sum = a + b
-            print(f"La suma de {a} y {b} es: {sum}")
-        case "-":
-            sub = a - b
-            print(f"La resta de {a} y {b} es: {sub}")
-        case "*":
-            mul = a * b
-            print(f"La multiplicacion de {a} y {b} es: {mul}")
-        case "/":
-            div = a / b
-            print(f"La division de {a} y {b} es: {div}")
-        case _:
-            print("Operacion no valida")
-
-a = int(input("Ingrese el primer numero: "))
-b = int(input("Ingrese el segundo numero: "))
-x = input("Ingrese la operacion a realizar: ")
-calculator(a,b,x)
+#Ejercicio 4: Determinacion del tipo de triangulo
+print("Exercise 4")
+for x in range (0,3):
+    lado = float(input("Ingrese el lado del triangulo: "))
+    if lado <= 0:
+        print("El lado del triangulo no puede ser menor o igual a 0")
+        break
+    else:
+        if x == 0:
+            lado1 = lado
+        elif x == 1:
+            lado2 = lado
+        elif x == 2:
+            lado3 = lado
+            if lado1 == lado2 and lado1 == lado3:
+                print("El triangulo es equilatero")
+            elif lado1 == lado2 or lado1 == lado3 or lado2 == lado3:
+                print("El triangulo es isosceles")
+            else:
+                print("El triangulo es escaleno")
