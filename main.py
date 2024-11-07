534fbf899ef6c179bfd3e321027ccb73ba01a50b
“joanomana” <joansebastianomasu@gmail.com>
2024-11-07 11:42:44 -0500

    Exercise 5

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-#Ejercicio 4: Determinacion del tipo de triangulo
-print("Exercise 4")
-for x in range (0,3):
-    lado = float(input("Ingrese el lado del triangulo: "))
-    if lado <= 0:
-        print("El lado del triangulo no puede ser menor o igual a 0")
-        break
-    else:
-        if x == 0:
-            lado1 = lado
-        elif x == 1:
-            lado2 = lado
-        elif x == 2:
-            lado3 = lado
-            if lado1 == lado2 and lado1 == lado3:
-                print("El triangulo es equilatero")
-            elif lado1 == lado2 or lado1 == lado3 or lado2 == lado3:
-                print("El triangulo es isosceles")
-            else:
-                print("El triangulo es escaleno")
+#Ejercicio 5: Dias de la semana
+print("Exercise 5")
+
+def day (x):
+    match x:
+        case 1:
+            print("The number 1 is Monday")
+        case 2: 
+            print("The number 2 is Tuesday")
+        case 3:
+            print("The number 3 is Wednesday")
+        case 4: 
+            print("The number 4 is Thursday")
+        case 5: 
+            print("The number 5 is Friday")
+        case 6: 
+            print("The number 6 is Saturday")
+        case 7:
+            print("The number 7 is Sunday")
+        case _:
+            print("Incorrect Number")
+day(x=int(input("Enter a number between 1 and 7 to display the day of the week: ")))
