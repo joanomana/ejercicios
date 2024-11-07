7f047ee4637fc4830ee52b529129b43032efd79f
“joanomana” <joansebastianomasu@gmail.com>
2024-11-07 11:17:16 -0500

    Exercise 3

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-#Ejercicio 2: Calificacion de una nota
-print("EXercise 2")
+#Ejercicio 3: Calculadora Basica
+def calculator (a,b,x):
+    match x:
+        case "+":
+            sum = a + b
+            print(f"La suma de {a} y {b} es: {sum}")
+        case "-":
+            sub = a - b
+            print(f"La resta de {a} y {b} es: {sub}")
+        case "*":
+            mul = a * b
+            print(f"La multiplicacion de {a} y {b} es: {mul}")
+        case "/":
+            div = a / b
+            print(f"La division de {a} y {b} es: {div}")
+        case _:
+            print("Operacion no valida")
 
-def calificacion(nota):
-    
-    if nota >= 60:
-        print("Aprobado")
-    else:
-        print("Reprobado")
-
-calificacion(nota= float(input("Ingrese la nota para verificar si es Aprobado o Reprobado: ")))
+a = int(input("Ingrese el primer numero: "))
+b = int(input("Ingrese el segundo numero: "))
+x = input("Ingrese la operacion a realizar: ")
+calculator(a,b,x)
