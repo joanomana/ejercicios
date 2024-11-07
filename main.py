e5d043e95e3e3c168676b553c55a479217bd8a62
“joanomana” <joansebastianomasu@gmail.com>
2024-11-07 11:54:25 -0500

    Exercise 6

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
-#Ejercicio 5: Dias de la semana
-print("Exercise 5")
+#Ejercicio 6: Juego de adivinanzas de numeros
+print("Exercise 6")
 
-def day (x):
-    match x:
-        case 1:
-            print("The number 1 is Monday")
-        case 2: 
-            print("The number 2 is Tuesday")
-        case 3:
-            print("The number 3 is Wednesday")
-        case 4: 
-            print("The number 4 is Thursday")
-        case 5: 
-            print("The number 5 is Friday")
-        case 6: 
-            print("The number 6 is Saturday")
-        case 7:
-            print("The number 7 is Sunday")
-        case _:
-            print("Incorrect Number")
-day(x=int(input("Enter a number between 1 and 7 to display the day of the week: ")))
+import random
+
+x = random.randint(1,10)
+print("")
+print(x)
+number = int(input("Enter a number between 1 an 10 to try to guess the random number: "))
+
+if number < x:
+    print("The number you entered is lower, try again")
+elif number > x:
+    print("The number you entered is higher, try again")
+else: 
+    print("You did it, you guessed the number")
