c663cfb33e99ec3476549876bbc17afdc656866f
“joanomana” <joansebastianomasu@gmail.com>
2024-11-07 10:48:43 -0500

    Exercise 1

---
+++ main.py
@@ -0,0 +1,9 @@
+#Ejercicio 1: Verificacion de numeros pares e impares
+print("EXercise 1")
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("The number is even")
+else: 
+    print("The number is odd")
