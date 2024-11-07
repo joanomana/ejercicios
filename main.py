99501a24da952bf2801671554cd8e0de9495c5c2
“joanomana” <joansebastianomasu@gmail.com>
2024-11-07 12:48:54 -0500

    Exercise 11

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-#Ejercicio 10: Clasificacion de notas
-print("Exercise 10")
+#Ejercicio 11: Conversion de temperaturas
+print("Exercise 11")
 
-grade = int(input("Please enter a rating: "))
-
-if grade >=90 and grade <=100:
-    print("A")
-elif grade >=80 and grade <=89:
-    print("B")
-elif grade >=70 and grade <=79:
-    print("C")
-elif grade >=60 and grade <=69:
-    print("D") 
-else:
-    print("F")
+def convertir_temperatura(temp,x):
+    match x:
+        case "c":
+            new_temp = temp * (9/5)+32
+            print(f"The temperature in Fahrenheit is: {new_temp}")
+        case "f":
+            new_temp = (temp - 32) * (5/9)
+            print(f"The temperature in Celsius is: {new_temp}")
+        case _:
+            print("Invalid option")
+temp = int(input("Enter the temperature: "))
+x = input("Enter the unit of temperature (c or f): ")
+convertir_temperatura(temp,x)
